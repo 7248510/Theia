e7c4c1695f75d4b6771fbe7771cb68f2bbdef144
Caleb <56181475+7248510@users.noreply.github.com>
2021-11-25 19:03:41 -0500

    Code cleanup

--- launch.js
+++ launch.js
@@ -6,12 +6,12 @@ let fs = require('fs');
 //https://blog.logrocket.com/how-to-make-http-requests-like-a-pro-with-axios/
 let array = fs.readFileSync('test.txt').toString().replace(/\r\n/g,'\n').split('\n'); //When reading the file sometimes \r gets attached this regex removes \r & \n
 let placeHolder = []; //Used in tracking the amount of spaces
-for (i = 0; i < array.length; i++) //Allocating space.
-  {
-      placeHolder.push(i); //Push numbers instead of strings so you can loop through them.(Fixing this issue was an all day. This is the only way I figured out how to time the requests)
-  }
+for (let i = 0; i < array.length; i++) //Allocating space.
+{
+    placeHolder.push(i); //Push numbers instead of strings so you can loop through them.(Fixing this issue was an all day. This is the only way I figured out how to time the requests)
+}
 const sleep = require("./sleep.js"); //Calling sleep js
-function target(url) 
+function target(url)
 {
     const axios = require('axios');
     const cheerio = require('cheerio');
@@ -22,29 +22,28 @@ function target(url)
     };
     console.log("Current URL: " + url)
     axios.get(url,options)
-    .then((response) => {
-        let jsonStore = {};
-        const $ = cheerio.load(response.data); //Response.data is the data from the response. Save this as $ and call cheerio to load the data   
-        let title = $('title').text();
-        let httpCode = response.statusText;
-        //I'm making this a json because I eventually plan to implement Mongo for storage/Maybe I'll make a rest API
-        jsonStore = {
-            "Title ": title,
-            "HTTP status" : httpCode
-        }
-        let final = JSON.stringify(jsonStore);
-        //If an error occurs here jsonStore is invalid. In order to parse it has to be valid. You can run the stringify function to ensure a proper json
-        final = JSON.parse(final)
-        console.log(final); 
-    })
-    .catch(function(err){
-        console.log(err); //Catch an error
+        .then((response) => {
+            const $ = cheerio.load(response.data); //Response.data is the data from the response. Save this as $ and call cheerio to load the data
+            let title = $('title').text();
+            let httpCode = response.statusText;
+            //I'm making this a json because I eventually plan to implement Mongo for storage/Maybe I'll make a rest API
+            let jsonStore = {
+                "Title ": title,
+                "HTTP status" : httpCode
+            }
+            let final = JSON.stringify(jsonStore);
+            //If an error occurs here jsonStore is invalid. In order to parse it has to be valid. You can run the stringify function to ensure a proper json
+            final = JSON.parse(final)
+            console.log(final);
+        })
+        .catch(function(err){
+            console.log(err); //Catch an error
         });
 }
 const launch = async () => {
-    for (const loopControl of placeHolder) { //The reason for placeholder is array holds strings and you cannot loop through URLS. 
-    target(array[loopControl]); 
-    await sleep.sleep(2000) //Every two seconds execute 
+    for (const loopControl of placeHolder) { //The reason for placeholder is array holds strings and you cannot loop through URLS.
+        target(array[loopControl]);
+        await sleep.sleep(2000) //Every two seconds execute
     }
 }
-launch()
+launch();
